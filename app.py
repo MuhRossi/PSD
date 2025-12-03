616ac9de12f58f3a37bc06601181b38e64b94776
muhrossi <117130543+MuhRossi@users.noreply.github.com>
2025-12-03 21:12:06 +0700

    Add Streamlit import to app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit as st
 from tabulate import tabulate
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
